78701c3d9398a79ff451ddc38e5c76eb94c50f7a
acerudyn <acerudyn@yahoo.com>
2018-08-23 16:18:49 +0700

    add reguler sale confirm & reguler sale tip

--- app/src/main/java/com/seeudin/ipos_beps/RegulerSaleConfirmActivity.kt
+++ app/src/main/java/com/seeudin/ipos_beps/RegulerSaleConfirmActivity.kt
@@ -1,12 +1,24 @@
 package com.seeudin.ipos_beps
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class RegulerSaleConfirmActivity : AppCompatActivity() {
+
+    internal lateinit var imgViewBack: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reguler_sale_confirm)
+
+        imgViewBack = findViewById<View>(R.id.imgViewBack) as ImageView
+
+        imgViewBack.setOnClickListener {
+            val intent = Intent(this, RegulerSaleActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
