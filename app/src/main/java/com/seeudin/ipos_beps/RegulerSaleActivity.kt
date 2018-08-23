fde71f499254e808f39e1c7ab74b3f89387fb4de
acerudyn <acerudyn@yahoo.com>
2018-08-23 16:17:33 +0700

    edit reguler sale activity

--- app/src/main/java/com/seeudin/ipos_beps/RegulerSaleActivity.kt
+++ app/src/main/java/com/seeudin/ipos_beps/RegulerSaleActivity.kt
@@ -4,20 +4,35 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 
 class RegulerSaleActivity : AppCompatActivity() {
 
     internal lateinit var imgViewBack: ImageView
+    internal lateinit var btnConfirm: Button
+    internal lateinit var btnConfirmTip: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reguler_sale)
 
         imgViewBack = findViewById<View>(R.id.imgViewBack) as ImageView
+        btnConfirm = findViewById<View>(R.id.btnConfirm) as Button
+        btnConfirmTip = findViewById<View>(R.id.btnConfirmTip) as Button
 
         imgViewBack.setOnClickListener {
             val intent = Intent(this, VoidActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnConfirm.setOnClickListener {
+            val intent = Intent(this, RegulerSaleConfirmActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnConfirmTip.setOnClickListener {
+            val intent = Intent(this, RegulerSaleTipActivity::class.java)
             startActivity(intent)
         }
     }
