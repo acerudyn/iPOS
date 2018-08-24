60acf84783321cce32f848e114efd9c628f484f7
acerudyn <acerudyn@yahoo.com>
2018-08-24 14:39:44 +0700

    add trx success

--- app/src/main/java/com/seeudin/ipos_beps/TrxSuccessActivity.kt
+++ app/src/main/java/com/seeudin/ipos_beps/TrxSuccessActivity.kt
@@ -1,12 +1,24 @@
 package com.seeudin.ipos_beps
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class TrxSuccessActivity : AppCompatActivity() {
+
+    internal lateinit var imgViewBack: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trx_success)
+
+        imgViewBack = findViewById<View>(R.id.imgViewBack) as ImageView
+
+        imgViewBack.setOnClickListener {
+            val intent = Intent(this, RegulerSaleConfirmActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
