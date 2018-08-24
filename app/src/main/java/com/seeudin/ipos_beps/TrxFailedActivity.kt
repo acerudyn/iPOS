3093eb67509e2effffdfd51dadb6f27cd4016f57
acerudyn <acerudyn@yahoo.com>
2018-08-24 14:39:13 +0700

    add trx failed

--- app/src/main/java/com/seeudin/ipos_beps/TrxFailedActivity.kt
+++ app/src/main/java/com/seeudin/ipos_beps/TrxFailedActivity.kt
@@ -1,12 +1,24 @@
 package com.seeudin.ipos_beps
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class TrxFailedActivity : AppCompatActivity() {
+
+    internal lateinit var imgViewBack: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trx_failed)
+
+        imgViewBack = findViewById<View>(R.id.imgViewBack) as ImageView
+
+        imgViewBack.setOnClickListener {
+            val intent = Intent(this, RegulerSaleTipActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
