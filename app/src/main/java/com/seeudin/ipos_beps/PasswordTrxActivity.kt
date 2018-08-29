64f760f449356b3ce707cc03403816c8a321def0
acerudyn <acerudyn@yahoo.com>
2018-08-29 10:27:50 +0700

    add password void & void main activity

--- app/src/main/java/com/seeudin/ipos_beps/PasswordTrxActivity.kt
+++ app/src/main/java/com/seeudin/ipos_beps/PasswordTrxActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
@@ -12,6 +13,7 @@ class PasswordTrxActivity : AppCompatActivity() {
 
     internal lateinit var imgViewBack: ImageView
     internal lateinit var pwdEntry: EditText
+    internal lateinit var btnEnter: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,19 +21,24 @@ class PasswordTrxActivity : AppCompatActivity() {
 
         imgViewBack = findViewById<View>(R.id.imgViewBack) as ImageView
         pwdEntry = findViewById<View>(R.id.editTextPwd) as EditText
+        btnEnter = findViewById<View>(R.id.btnEnter) as Button
 
         imgViewBack.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-        //pwdEntry = findViewById<View>(R.id.editTextPwd) as EditText
-        pwdEntry.setOnClickListener { str ->
-            if (str.toString() == "Bahana12") {
+        btnEnter.setOnClickListener {
+            if (pwdEntry.text.toString() == "Bahana911") {
                 Toast.makeText(this@PasswordTrxActivity, "SUCCESS", Toast.LENGTH_SHORT).show()
+                pwdEntry.setText(null)
+                val intent = Intent(this, VoidActivity::class.java)
+                startActivity(intent)
             } else {
                 Toast.makeText(this@PasswordTrxActivity, "INVALID", Toast.LENGTH_SHORT).show()
                 pwdEntry.setText(null)
+                val intent = Intent(this, PinInvalidActivity::class.java)
+                startActivity(intent)
             }
         }
 
